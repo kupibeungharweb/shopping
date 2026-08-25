4a07574e9faafc66763190620af9500109a8d0e3
Kupi Beunghar <kupibeunghar@gmail.com>
2026-08-25 19:08:39 +0700

    Update vite.config.js

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/shopping/",
-  build: {
-    outDir: "../../dist/apps/web",
-    emptyOutDir: true,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
